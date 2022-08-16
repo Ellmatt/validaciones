ff547bd9f11828bf8bce6c343b38c72dd8ab1670
matias <pagosptcmatias@gmail.com>
2022-08-16 18:36:54 -0300

    agregamos dev

---
+++ js/validaciones.js
@@ -0,0 +1,9 @@
+export function cantidadCaracteres(input){
+    if(input.value.length >= 3 ){
+        console.log('dato valido')
+    }else{
+        console.log('dato invalido')
+    }
+  
+    
+  }
